1551d179e84b3f3ba435be9a3130778777b1ee29
mytam201 <tamdinh788@gmail.com>
2022-10-14 22:26:04 -0700

    test

--- file.js
+++ file.js
@@ -1,10 +1,21 @@
-//tam: create 3 variables by id name in html
-let addToDoButton = document.getElementById('new-task-submit');
-let toDoContainer = document.getElementById('new-task-form');
-let inputField = document.getElementById('new-task-input');
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector('#new-task-input');
+    const list_el = document.querySelector("#tasks");
 
-addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p')
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+        task_content_el.innerText = task;
+
+        task_el.appendChild(task_content_el);
+        list_el.appendChild(task_el);
+    })
 })
